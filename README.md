84eee43050cdeec0e53283a782a54ca29023904a
shuvro <shuvro.dhrubo@gmail.com>
2020-04-12 17:13:41 +0600

    init commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Tic-Tac-Toe client written with React + Redux
+
+Backend server at https://github.com/shuvro/mern-tic-tac-toe-server.git
+ 
+### Start
+
+````bash
+yarn
+````
+
+````bash
+yarn build:dev
+````
+
+````bash
+yarn serve:dev
+````
+
+````bash
+http://localhost:3000/
+````
---
+++ src/components/App.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import Loader from './Loader';
+import Grid from './Grid/index';
+import ActionsLog from './ActionsLog';
+
+class App extends Component {
+
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <div className='container'>
+                <Loader>
+                    <Grid/>
+                </Loader>
+
+                <h1 className='title'>TIC TAC TOE</h1>
+                <h2 className="server-messages">{this.props.message}</h2>
+
+                <ActionsLog/>
+            </div>
+        );
+    }
+}
+
+export default connect(
+    state => ({ message: state.game.message })
+)(App);
